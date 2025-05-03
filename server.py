ffec255ef5c4a6d3c70b833c31bb831a9c24d663
sam altman <99407645+prkoli@users.noreply.github.com>
2025-05-03 22:02:39 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,11 @@
+from xmlrpc.server import SimpleXMLRPCServer
+
+def factorial(n):
+	if n==0 or n==1:
+		return 1
+	else:
+		return n*(factorial(n-1))
+
+server = SimpleXMLRPCServer(("localhost",8000))
+server.register_function(factorial,"calculate_factorial")
+server.serve_forever()
