8302592609fe95e82483ccf9b27744ccc2bbc17e
sam altman <99407645+prkoli@users.noreply.github.com>
2025-05-04 16:57:36 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,22 @@
 from xmlrpc.server import SimpleXMLRPCServer
 
 def factorial(n):
-	if n==0 or n==1:
-		return 1
-	else:
-		return n*(factorial(n-1))
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
-server = SimpleXMLRPCServer(("localhost",8000))
-server.register_function(factorial,"calculate_factorial")
+server = SimpleXMLRPCServer(("localhost", 8000))
+server.register_function(factorial, "calculate_factorial")
+print("Server is running on port 8000...")
 server.serve_forever()
+
 
 import xmlrpc.client
 
 server = xmlrpc.client.ServerProxy("http://localhost:8000")
 
 number = int(input("Enter an integer to compute factorial for: "))
-result = server.compute_factorial(number)
+result = server.calculate_factorial(number)
 print(f"The factorial of {number} is {result}")
+
