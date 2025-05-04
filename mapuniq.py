ace7a5b344262b4a978f69efdc45615c11f31995
sam altman <99407645+prkoli@users.noreply.github.com>
2025-05-04 18:18:27 +0530

    Add files via upload

---
+++ mapuniq.py
@@ -0,0 +1,33 @@
+import os
+import re
+import pandas as pd
+os.listdir()
+def mapper(review):
+    review = review.strip()
+    review = review.lower()
+    review = re.findall("[a-z]+", review)
+    return review
+
+def reduce(review):
+    word_count = {}
+    for word in review:
+        word_count[word] = review.count(word)
+    return word_count
+
+def map_reduce(file_name):
+    with open(file_name, "r") as file:
+        review = file.read()
+        
+    mapped = mapper(review)
+    reducer = reduce(mapped)
+    
+    x1, x2 = [], []
+    for word, count in reducer.items():
+        x1.append(word)
+        x2.append(count)
+    return x1, x2
+
+y1, y2 = map_reduce("sample.txt")
+
+dict1 = {"count":y2}
+pd.DataFrame(dict1, index=y1)
